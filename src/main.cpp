74d398a25763027e34dcb0ac8fc7d68fcbf36ba7
SAMET KAŞMER <185601433+Layellie@users.noreply.github.com>
2026-07-14 15:35:22 +0300

    Add Application main-menu loop, completing the Foundation phase

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,7 @@
-#include <iostream>
-
-#include "ConsoleUI.h"
+#include "Application.h"
 
 int main() {
-    ConsoleUI ui;
-
-    ui.printHeader("CONSOLEUI TEST");
-    ui.printLine("0 ile 5 arasinda bir sayi girin (once gecersiz bir deger deneyin):");
-    const int choice = ui.readMenuChoice(0, 5);
-    ui.printLine("Sectiginiz deger: " + std::to_string(choice));
-
-    const std::string name = ui.readLine("Adiniz: ");
-    ui.printLine("Merhaba, " + name + "!");
-
+    Application app;
+    app.run();
     return 0;
 }
