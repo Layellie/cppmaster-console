bb827f5cb81e54b0e0e786b90f9d5bc54ea7a793
SAMET KAŞMER <samet.kasmer@ogr.dpu.edu.tr>
2026-07-17 02:19:44 +0300

    feat(content): add questions for topics 31-34 (ids 653-768)

--- src/QuestionContent.h
+++ src/QuestionContent.h
@@ -6,3 +6,4 @@ struct Question;
 
 void appendSection2Questions(std::vector<Question>& questions);
 void appendSection3Questions(std::vector<Question>& questions);
+void appendSection4Questions(std::vector<Question>& questions);
