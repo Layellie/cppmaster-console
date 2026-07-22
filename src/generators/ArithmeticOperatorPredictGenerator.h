2e37a970b8bd88cc11ab19c78d6c83bea1817330
SAMET KAŞMER <185601433+Layellie@users.noreply.github.com>
2026-07-22 14:56:16 +0300

    feat(generators): add ArithmeticOperatorPredictGenerator (topic 11)

---
+++ src/generators/ArithmeticOperatorPredictGenerator.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "IQuestionGenerator.h"
+
+class ArithmeticOperatorPredictGenerator final : public IQuestionGenerator {
+public:
+    [[nodiscard]] std::string_view id() const noexcept override;
+    [[nodiscard]] int topicId() const noexcept override;
+    [[nodiscard]] std::optional<GeneratedQuestion> generate(
+        std::mt19937_64& randomEngine, GenerationStage stage) const override;
+};
